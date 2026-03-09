a79e9f8ddd991491f966ca4065f6d92c6c633692
fizakim <fizakim@gmail.com>
2026-03-09 23:59:11 +0000

    Implemented django auth

--- app/users/views.py
+++ app/users/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, TemplateView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('users:login')
     template_name = 'users/signup.html'
 
-class AccountOverviewView(TemplateView):
+class AccountOverviewView(LoginRequiredMixin, TemplateView):
     template_name = 'users/account_overview.html'
