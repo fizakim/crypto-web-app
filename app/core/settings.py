fbde4d000d8dbe82632adb2b2a35654ef113e75b
fizakim <fizakim@gmail.com>
2026-03-12 13:54:48 +0000

    Implement a very basic blockchain viewer page.

--- app/core/settings.py
+++ app/core/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'styles']
 
 # Auth routing
 LOGIN_REDIRECT_URL = 'users:account_overview'
--- app/crypto/views.py
+++ app/crypto/views.py
@@ -1,7 +1,34 @@
 from django.views.generic import TemplateView
+from .models import Cryptocurrency, Block
 
 class BlockchainView(TemplateView):
     template_name = 'crypto/blockchain_viewer.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        cryptos = Cryptocurrency.objects.all()
+        context['cryptocurrencies'] = cryptos
+        selected_crypto_symbol = self.request.GET.get('network')
+        
+        if selected_crypto_symbol:
+            active_crypto = Cryptocurrency.objects.filter(symbol=selected_crypto_symbol).first()
+        else:
+            active_crypto = cryptos.first() if cryptos.exists() else None
+            
+        context['active_crypto'] = active_crypto
+        
+        if active_crypto:
+            blockchain = active_crypto.blockchain_set.first()
+            if blockchain:
+                blocks = Block.objects.filter(blockchain=blockchain).order_by('-height')[:50] # Get latest 50 blocks
+                context['blocks'] = blocks
+            else:
+                context['blocks'] = []
+        else:
+            context['blocks'] = []
+            
+        return context
 
 class MiningView(TemplateView):
     template_name = 'crypto/mining.html'
