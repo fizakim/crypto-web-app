102bc7c73a02ab100141100e76fb551f8468ed61
Douglas Ormrod <3019255O@student.gla.ac.uk>
2026-03-26 11:56:03 +0000

    added css for login page

--- app/core/settings.py
+++ app/core/settings.py
@@ -121,7 +121,13 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
-]
+    ]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = [
+    BASE_DIR / 'media',
+    ]
+
 
 # Auth routing
 LOGIN_REDIRECT_URL = 'pages:home'
