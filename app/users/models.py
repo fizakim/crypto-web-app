4d82b5f5b312cc20fd406326ea83e6cb84a5bfc5
siddboredasf <xynoscraft@gmail.com>
2026-03-26 13:15:49 +0000

    Add homepage, login, signup and account overview updates

--- app/users/models.py
+++ app/users/models.py
@@ -1,1 +1,30 @@
+from django.conf import settings
 from django.db import models
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=18, decimal_places=2, default=10000.00)
+
+    def __str__(self):
+        return self.user.username
+
+
+class PortfolioHolding(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='holdings')
+    symbol = models.CharField(max_length=20)
+    blockchain = models.CharField(max_length=30, blank=True)
+    amount = models.DecimalField(max_digits=30, decimal_places=10, default=0)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.symbol}'
+
+
+class WalletSnapshot(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='wallet_snapshots')
+    total_value = models.DecimalField(max_digits=18, decimal_places=2)
+    period_label = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.total_value}'
--- app/users/urls.py
+++ app/users/urls.py
@@ -5,8 +5,20 @@ from .views import SignUpView, AccountOverviewView
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html', next_page='users:account_overview'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='pages:home'), name='logout'),
+    path(
+        'login/',
+        auth_views.LoginView.as_view(
+            template_name='users/login.html',
+            redirect_authenticated_user=True,
+            next_page='pages:home'
+        ),
+        name='login'
+    ),
+    path(
+        'logout/',
+        auth_views.LogoutView.as_view(next_page='users:login'),
+        name='logout'
+    ),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('account/', AccountOverviewView.as_view(), name='account_overview'),
 ]
